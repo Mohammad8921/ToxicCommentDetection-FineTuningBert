3943ed15b13710475c2751d7f5b467e963796f65
unknown <lashgari.m@live.com>
2023-01-28 18:23:41 +0330

    Update

---
+++ Codes/models.py
@@ -0,0 +1,163 @@
+!pip install transformers==3.0.0
+!pip install emoji
+
+import gc
+import torch
+import torch.nn as nn
+from transformers import BertModel
+
+class BERT_CNN(nn.Module):
+
+    def __init__(self):
+        super(BERT_CNN, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.conv = nn.Conv2d(in_channels=13, out_channels=13, kernel_size=(3, 768), padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=1)
+        self.dropout = nn.Dropout(0.1)
+        self.fc = nn.Linear(442, 3)
+        self.flat = nn.Flatten()
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, sent_id, mask):
+        _, _, all_layers = self.bert(sent_id, attention_mask=mask, output_hidden_states=True)
+        # all_layers  = [13, 32, 64, 768]
+        x = torch.transpose(torch.cat(tuple([t.unsqueeze(0) for t in all_layers]), 0), 0, 1)
+        del all_layers
+        gc.collect()
+        torch.cuda.empty_cache()
+        x = self.pool(self.dropout(self.relu(self.conv(self.dropout(x)))))
+        x = self.fc(self.dropout(self.flat(self.dropout(x))))
+        return self.softmax(x)
+
+class BERT_CNN_1D(nn.Module):
+
+  def __init__(self):
+    super(BERT_CNN_1D, self).__init__()
+    self.bert = BertModel.from_pretrained('bert-base-uncased')
+    self.conv = nn.Conv1d(in_channels=768, out_channels=64, kernel_size=3, padding="same")
+    self.pool = nn.MaxPool1d(kernel_size=3, stride=1)
+    self.relu = nn.ReLU()
+    self.dropout = nn.Dropout(0.1)
+    self.fc = nn.Linear(2176, 2) ## 64*34 = 2176
+    self.flat = nn.Flatten()
+    self.softmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, sent_id, mask): 
+    last_hidden_state, _ = self.bert(sent_id, attention_mask=mask)
+    x = last_hidden_state
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    x = self.dropout(x)
+    x = torch.transpose(x, 1, 2)
+    x = self.pool(self.dropout(self.relu(self.conv(x))))
+    x = self.fc(self.dropout(self.flat(self.dropout(x))))
+    return self.softmax(x)
+
+class BERT_LSTM(nn.Module):
+
+  def __init__(self):
+    super(BERT_LSTM, self).__init__()
+    self.bert = BertModel.from_pretrained('bert-base-uncased')
+    self.lstm = nn.LSTM(768, 768, 1)
+    self.relu = nn.ReLU()
+    self.dropout = nn.Dropout(0.1)
+    self.dropout2 = nn.Dropout(0.3) # For fully connected layer
+    self.fc = nn.Linear(27648, 2) # 36*768 = 27648
+    self.flat = nn.Flatten()
+    self.softmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, sent_id, mask): 
+    last_hidden_state, _ = self.bert(sent_id, attention_mask=mask)
+    x = last_hidden_state
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    x,_ = self.lstm(self.dropout(x))
+    x = self.fc(self.dropout2(self.flat(self.dropout(x))))
+    return self.softmax(x)
+
+class BERT_LSTM_CNN(nn.Module):
+
+  def __init__(self):
+    super(BERT_LSTM_CNN, self).__init__()
+    self.bert = BertModel.from_pretrained('bert-base-uncased')
+    self.lstm = nn.LSTM(768, 768, 1)
+    self.conv = nn.Conv1d(in_channels=768, out_channels=64, kernel_size=3, padding="same")
+    self.pool = nn.MaxPool1d(kernel_size=3, stride=1)
+    self.relu = nn.ReLU()
+    self.dropout = nn.Dropout(0.1)
+    self.fc = nn.Linear(2176, 2) #64*34 = 2176
+    self.flat = nn.Flatten()
+    self.softmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, sent_id, mask): 
+    last_hidden_state, _ = self.bert(sent_id, attention_mask=mask)
+    x = last_hidden_state
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    x = self.lstm(self.dropout(x))
+    x = x[0] # output of lstm is a tuple.
+    x = torch.transpose(x, 1, 2)
+    x = self.pool(self.dropout(self.relu(self.conv(x))))
+    x = self.fc(self.dropout(self.flat(self.dropout(x))))
+    return self.softmax(x)
+
+class BERT_CNN_LSTM(nn.Module):
+    
+  def __init__(self):
+    super(BERT_CNN_LSTM, self).__init__()
+    self.bert = BertModel.from_pretrained('bert-base-uncased')
+    self.lstm = nn.LSTM(64, 64, 1)
+    self.conv = nn.Conv1d(in_channels=768, out_channels=64, kernel_size=3, padding="same")
+    self.pool = nn.MaxPool1d(kernel_size=3, stride=1)
+    self.relu = nn.ReLU()
+    self.dropout = nn.Dropout(0.1)
+    self.fc = nn.Linear(2176, 2) # 64*34 = 2176
+    self.flat = nn.Flatten()
+    self.softmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, sent_id, mask): 
+    last_hidden_state, _ = self.bert(sent_id, attention_mask=mask)
+    x = last_hidden_state
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    x = torch.transpose(x, 1, 2)
+    x = self.pool(self.dropout(self.relu(self.conv(self.dropout(x)))))
+    x = torch.transpose(x, 1, 2)
+    x,_ = self.lstm(self.dropout(x))
+    x = self.fc(self.dropout(self.flat(self.dropout(x))))
+    return self.softmax(x)
+
+class BERT_2CNN_1D(nn.Module):
+
+  def __init__(self):
+    super(BERT_2CNN_1D, self).__init__()
+    self.bert = BertModel.from_pretrained('bert-base-uncased')
+    self.conv = nn.Conv1d(in_channels=768, out_channels=64, kernel_size=3, padding="same")
+    self.conv2 = nn.Conv1d(in_channels=64, out_channels=32, kernel_size=3, padding="same")
+    self.pool = nn.MaxPool1d(kernel_size=3, stride=1)
+    self.relu = nn.ReLU()
+    self.dropout = nn.Dropout(0.1)
+    self.fc = nn.Linear(1024, 2) # 32*(36-2-2) = 1024
+    self.flat = nn.Flatten()
+    self.softmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, sent_id, mask): 
+    last_hidden_state, _ = self.bert(sent_id, attention_mask=mask)
+    x = last_hidden_state
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    x = torch.transpose(x, 1, 2)
+    x = self.pool(self.dropout(self.relu(self.conv(self.dropout(x)))))
+    x = self.pool(self.dropout(self.relu(self.conv2(self.dropout(x)))))
+    x = self.fc(self.dropout(self.flat(self.dropout(x))))
+    return self.softmax(x)
---
+++ Codes/preprocessing.py
@@ -0,0 +1,83 @@
+import re
+import string
+import pandas as pd
+import emoji
+from transformers import BertTokenizer
+
+DATASET_PATH = '/content/drive/MyDrive/NLP-project/dataset/train.csv'
+
+def balance_dataset(dataset):
+    min_len = (dataset['label']==1).sum()
+    sampled_from_0s = dataset[dataset['label']==0].sample(n = 2*min_len, random_state=42)
+    return pd.concat([dataset[dataset['label']==1], sampled_from_0s])
+
+def read_dataset(path):
+    dataset = pd.read_csv(path)
+    dataset = dataset[~pd.isna(dataset['comment_text'])]
+    dataset['label'] = dataset.values[:,2:].sum(axis=1) > 0
+    dataset['label'] = dataset['label'].astype(int)
+    balanced_dataset = balance_dataset(dataset)
+    return balanced_dataset['comment_text'].tolist(), balanced_dataset['label']
+
+def data_process(data, labels):
+    input_ids = []
+    attention_masks = []
+    bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    for sentence in data:
+        bert_inp = bert_tokenizer.__call__(sentence, max_length=36,
+                                           padding='max_length', pad_to_max_length=True,
+                                           truncation=True, return_token_type_ids=False)
+
+        input_ids.append(bert_inp['input_ids'])
+        attention_masks.append(bert_inp['attention_mask'])
+    input_ids = np.asarray(input_ids)
+    attention_masks = np.array(attention_masks)
+    labels = np.array(labels)
+
+    return input_ids, attention_masks, labels
+
+def pre_process_dataset(values):
+    new_values = list()
+    # Emoticons
+    emoticons = [':-)', ':)', '(:', '(-:', ':))', '((:', ':-D', ':D', 'X-D', 'XD', 'xD', 'xD', '<3', '</3', ':\*',
+                 ';-)',
+                 ';)', ';-D', ';D', '(;', '(-;', ':-(', ':(', '(:', '(-:', ':,(', ':\'(', ':"(', ':((', ':D', '=D',
+                 '=)',
+                 '(=', '=(', ')=', '=-O', 'O-=', ':o', 'o:', 'O:', 'O:', ':-o', 'o-:', ':P', ':p', ':S', ':s', ':@',
+                 ':>',
+                 ':<', '^_^', '^.^', '>.>', 'T_T', 'T-T', '-.-', '*.*', '~.~', ':*', ':-*', 'xP', 'XP', 'XP', 'Xp',
+                 ':-|',
+                 ':->', ':-<', '$_$', '8-)', ':-P', ':-p', '=P', '=p', ':*)', '*-*', 'B-)', 'O.o', 'X-(', ')-X']
+
+    for value in values:
+        # Remove dots
+        text = value.replace(".", "").lower()
+        text = re.sub(r"[^a-zA-Z?.!,¿]+", " ", text)
+        users = re.findall("[@]\w+", text)
+        for user in users:
+            text = text.replace(user, "<user>")
+        urls = re.findall(r'(https?://[^\s]+)', text)
+        if len(urls) != 0:
+            for url in urls:
+                text = text.replace(url, "<url >")
+        for emo in text:
+            if emo in emoji.UNICODE_EMOJI:
+                text = text.replace(emo, "<emoticon >")
+        for emo in emoticons:
+            text = text.replace(emo, "<emoticon >")
+        numbers = re.findall('[0-9]+', text)
+        for number in numbers:
+            text = text.replace(number, "<number >")
+        text = text.replace('#', "<hashtag >")
+        text = re.sub(r"([?.!,¿])", r" ", text)
+        text = "".join(l for l in text if l not in string.punctuation)
+        text = re.sub(r'[" "]+', " ", text)
+        new_values.append(text)
+    return new_values
+
+def load_and_process(path):
+    data, labels = read_dataset(path)
+    num_of_labels = len(labels.unique())
+    input_ids, attention_masks, labels = data_process(pre_process_dataset(data), labels)
+
+    return input_ids, attention_masks, labels
